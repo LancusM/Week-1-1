b45f78cf797adbff9b23025a424409e6be3a172f
Lancus <llmaxwell21@amarillocollege.com>
2024-08-24 00:50:04 -0500

    Excercise 1 Finished (In the way I know how, which may not be correct.)

---
+++ Week-1-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Excercise 1, Console writes message and leaves it on screen for it to be read
+            Console.WriteLine("Welcome to ITSE1302");
+            Console.Read();
+        }
+    }
+}
